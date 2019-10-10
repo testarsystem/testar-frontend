59fb04dee249c1157961307723b7c9c6987a3e88
msuyumbaeva <msuyumbaeva@gmail.com>
2019-10-10 20:48:52 +0600

    register page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,23 @@
 <template>
     <div class="login row">
-        <h2>Вход</h2>
-        <div class="col m6 l3 s12 offset-m3" id="loginForm">
+        <h3>Вход</h3>
+        <div class="col m6 l3 s8 offset-m3 offset-s2" id="loginForm">
             <Alert type="danger" v-if="error"></Alert>
             <div class="row">
                 <div class="input-field col s12">
-                <input id="username" type="text" class="validate">
-                <label for="username">Логин или email</label>
+                    <input id="username" type="text" class="validate">
+                    <label for="username">Логин или email</label>
                 </div>
-            </div>
-            <div class="row">
                 <div class="input-field col s12">
-                <input id="password" type="password" class="validate">
-                <label for="password">Пароль</label>
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Пароль</label>
                 </div>
             </div>
             <div class="row">
                 <button class="waves-effect waves-light btn" id="loginFormBtn">Войти</button>
+            </div>
+            <div class="row">
+                <p>Нет учетной записи? <router-link to="register">Зарегистрироваться</router-link></p>
             </div>
         </div>
     </div>
@@ -40,10 +41,16 @@ export default {
 
 <style>
 #loginForm{
-    margin-left: 37.5%;
     padding-top: 1.2rem;
     padding-bottom: 1.2rem;
 }
+
+@media screen and (min-width: 992px){
+    #loginForm {
+        margin-left: 37.5%;
+    }
+}
+
 #loginFormBtn
 {
     margin: 1.2rem auto;
---
+++ src/views/Register.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="register row">
+        <h3>Регистрация</h3>
+        <div class="col m6 l6 s8 offset-m3 offset-l3 offset-s2" id="registerForm">
+            <Alert type="danger" v-if="error"></Alert>
+            <div class="row">
+                <div class="input-field col s12 l6">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                </div>
+                <div class="input-field col s12 l6">
+                    <input id="username" type="text" class="validate">
+                    <label for="username">Логин</label>
+                </div>
+                <div class="input-field col s12 l6">
+                    <input id="firstName" type="text" class="validate">
+                    <label for="firstName">Имя</label>
+                </div>
+                <div class="input-field col s12 l6">
+                    <input id="lastName" type="text" class="validate">
+                    <label for="lastName">Фамилия</label>
+                </div>
+                <div class="input-field col s12 l6">
+                    <input id="password" type="password" class="validate">
+                    <label for="password">Пароль</label>
+                </div>
+                <div class="input-field col s12 l6">
+                    <input id="passwordConfirm" type="password" class="validate">
+                    <label for="passwordConfirm">Подтвердите пароль</label>
+                </div>
+            </div>
+            <div class="row">
+                <button class="waves-effect waves-light btn" id="registerFormBtn">Зарегистрироваться</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Alert from '@/components/Alert.vue'
+
+export default {
+    name: "RegisterView",
+    components : {
+        Alert
+    },
+    data(){
+        return{
+            error: ""
+        }
+    }
+}
+</script>
+
+<style>
+#registerForm{
+    padding-top: 1.2rem;
+    padding-bottom: 1.2rem;
+}
+#registerFormBtn
+{
+    margin: 1.2rem auto;
+}
+</style>
