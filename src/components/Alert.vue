0d68ae10f9c97a47b2d193f39081b440f9dfb0d5
msuyumbaeva <msuyumbaeva@gmail.com>
2019-10-10 19:51:13 +0600

    login page

---
+++ src/components/Alert.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="alert" :class="type" role="alert">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: {
+      type: {
+          type: String,
+          default: "success"
+      }   
+  }
+}
+</script>
+
+<style scope>
+.alert {
+    padding: .75rem 1.25rem;
+    margin-bottom: 1rem;
+    border: 1px solid transparent;
+    border-radius: 2px;
+    
+}
+.success {
+    color: #155724;
+    background-color: #d4edda;
+    border-color: #c3e6cb;
+}
+.danger {
+    color: #721c24;
+    background-color: #f8d7da;
+    border-color: #f5c6cb;
+}
+.warning {
+    color: #856404;
+    background-color: #fff3cd;
+    border-color: #ffeeba;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="login row">
+        <h2>Вход</h2>
+        <div class="col m6 l3 s12 offset-m3" id="loginForm">
+            <Alert type="danger" v-if="error"></Alert>
+            <div class="row">
+                <div class="input-field col s12">
+                <input id="username" type="text" class="validate">
+                <label for="username">Логин или email</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                <input id="password" type="password" class="validate">
+                <label for="password">Пароль</label>
+                </div>
+            </div>
+            <div class="row">
+                <button class="waves-effect waves-light btn" id="loginFormBtn">Войти</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Alert from '@/components/Alert.vue'
+
+export default {
+    name: "LoginView",
+    components : {
+        Alert
+    },
+    data(){
+        return{
+            error: ""
+        }
+    }
+}
+</script>
+
+<style>
+#loginForm{
+    margin-left: 37.5%;
+    padding-top: 1.2rem;
+    padding-bottom: 1.2rem;
+}
+#loginFormBtn
+{
+    margin: 1.2rem auto;
+}
+</style>
