6a342e54f933571b10a1bdccd5cc10ea2b623882
msuyumbaeva <msuyumbaeva@gmail.com>
2019-11-12 18:27:58 +0600

    list of tests

---
+++ src/components/TestsCollection.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="tests-collection collection">
+    <li class="collection-item avatar" v-for="test in tests" :key="test.Id">      
+      <div class="title">
+        {{test.title}}
+      </div>
+      <p class="secondary">
+        {{test.date}}
+      </p>
+      <p class="content">
+        {{test.description}}
+      </p>      
+      <a class="btn btn-small">Start competition</a>
+    </li>
+  </ul> 
+</template>
+
+<script>
+export default {
+  name: 'TestsCollection',
+  props: {
+    tests: Array
+  }
+}
+</script>
+
+<style>
+.tests-collection .collection-item.avatar {
+  padding: 20px !important;
+}
+
+.tests-collection .title {
+  font-size: 1.64rem !important;
+  line-height: 110%;
+}
+
+.tests-collection .title:hover{
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.tests-collection {
+  margin: 2rem 0 1rem 0;
+}
+
+.tests-collection .content, .tests-collection .secondary {
+  margin: .5rem 0 1rem 0!important;
+}
+
+.tests-collection .secondary {
+  margin: .5rem 0 !important;
+  font-size: .9rem !important;
+  color: #546e7a ;
+}
+</style>
--- src/views/Tests/Tests.vue
+++ src/views/Tests/Tests.vue
@@ -2,11 +2,35 @@
   <div class="tests">    
     <h3>Tests</h3>
     <router-link to="/tests/new" class="waves-effect waves-light btn">New test</router-link>
+      <TestsCollection :tests="tests"/>  
   </div>
 </template>
 
 <script>
+import TestsCollection from '@/components/TestsCollection.vue'
+
 export default {
-  name: 'tests'
+  name: 'tests',
+  components: {
+    TestsCollection
+  },
+  data(){
+    return {
+      tests: [
+        {
+          id: 1,
+          title: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae quas, fugiat eos fugit maiores porro temporibus atque mollitia?',
+          description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non distinctio aliquid temporibus suscipit consequuntur, eius excepturi nam numquam voluptatem ex fugiat reiciendis velit blanditiis labore quas! Quibusdam, porro. Dolor harum et veniam aspernatur, facere eaque eveniet nemo dolore similique optio? Quae quas, fugiat eos fugit maiores porro temporibus atque mollitia?',
+          date: '01/01/2019'
+        },
+        {
+          id: 2,
+          title: 'My first test',
+          description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non distinctio aliquid temporibus suscipit consequuntur, eius excepturi nam numquam voluptatem ex fugiat reiciendis velit blanditiis labore quas! Quibusdam, porro. Dolor harum et veniam aspernatur, facere eaque eveniet nemo dolore similique optio? Quae quas, fugiat eos fugit maiores porro temporibus atque mollitia?',
+          date: '01/01/2018'
+        }
+      ]
+    }
+  }
 }
 </script>
